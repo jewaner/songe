0aa4864f74767fecb64b664f087c062cad6625d0
Jewaner <116263623+jewaner@users.noreply.github.com>
2023-05-28 10:56:19 +0200

    Hero modify

--- components/utility/Page.tsx
+++ components/utility/Page.tsx
@@ -40,21 +40,21 @@ function Page({ currentPage, meta: { title, desc }, children }: PageProps) {
         <meta name="description" content={desc} />
 
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://braydentw.io/" />
+        <meta property="og:url" content="https://songe-dev.netlify.app/" />
         <meta property="og:title" content={pageTitle} />
         <meta property="og:description" content={desc} />
         <meta
           property="og:image"
-          content="https://braydentw.io/static/misc/og.png"
+          content=""
         />
 
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://braydentw.io/" />
+        <meta property="twitter:url" content="https://songe-dev.netlify.app/" />
         <meta property="twitter:title" content={pageTitle} />
         <meta property="twitter:description" content={desc} />
         <meta
           property="twitter:image"
-          content="https://braydentw.io/static/misc/og.png"
+          content=""
         ></meta>
         <script
           async
